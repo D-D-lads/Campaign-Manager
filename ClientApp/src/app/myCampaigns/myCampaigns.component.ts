5ee872766247ec70653572252922d8ebecdb4423
Hunter Scheel <hunter.scheel@outlook.com>
2020-10-01 15:34:09 -0500

    edit working

--- ClientApp/src/app/myCampaigns/myCampaigns.component.ts
+++ ClientApp/src/app/myCampaigns/myCampaigns.component.ts
@@ -24,21 +24,6 @@ export class MyCampaignsComponent {
       },
     });
   }
-  deleteCampaign(id) {
-    swal
-      .fire({
-        title: "Do you want to delete?",
-        showDenyButton: true,
-        confirmButtonText: `Delete`,
-        denyButtonText: `Don't Delete`,
-      })
-      .then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-          this.deleteData(id);
-        }
-      });
-  }
   postData(value: string): Observable<Campaigns> {
     this.http
       .post<Campaigns>(this.baseUrl + "api/CampaignsController", {
@@ -63,9 +48,63 @@ export class MyCampaignsComponent {
       );
     return;
   }
+  deleteCampaign(id) {
+    swal
+      .fire({
+        title: "Do you want to delete?",
+        showDenyButton: true,
+        confirmButtonText: `Delete`,
+        denyButtonText: `Don't Delete`,
+      })
+      .then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.deleteData(id);
+        }
+      });
+  }
   deleteData(id: string) {
     this.http
       .delete<Campaigns>(this.baseUrl + `api/CampaignsController?id=${id}`)
+      .subscribe(
+        (result) => {
+          this.http
+            .get<Campaigns[]>(this.baseUrl + "api/CampaignsController")
+            .subscribe(
+              (result) => {
+                this.forecasts = result;
+              },
+              (error) => console.error(error)
+            );
+          return result;
+        },
+        (error) => {
+          console.error(error);
+          return error;
+        }
+      );
+    return;
+  }
+  editCampaign(id) {
+    swal.fire({
+      title: "New Campaign",
+      input: "text",
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to write something!";
+        } else {
+          this.editData(value, id);
+        }
+      },
+    });
+  }
+  editData(value: string, id: string) {
+    this.http
+      .put<Campaigns>(this.baseUrl + `api/CampaignsController?id=${id}`, {
+        id: id,
+        name: value,
+      })
       .subscribe(
         (result) => {
           this.http
